30272fdcd05d0a86e59a45895b9d4978de795b85
sydobd <obaidulhaq20@gmail.com>
2024-04-08 23:00:27 +0500

    created right layout widgets

--- main.py
+++ main.py
@@ -13,22 +13,34 @@ class Window(QWidget):
 
     def UI(self):
         self.mainDesign()
+        self.layouts()
 
     def mainDesign(self):
-        pass
+        self.employeeList = QListWidget()
+        self.btnNew = QPushButton("New")
+        self.btnUpdate = QPushButton("Update")
+        self.btnDelete = QPushButton("Delete")
+
 
     def layouts(self):
+        # Layouts #
         self.mainLayout = QHBoxLayout()
         self.leftLayout = QFormLayout()
         self.rightMainLayout = QVBoxLayout()
         self.rightTopLayout = QHBoxLayout()
         self.rightBottomLayout = QHBoxLayout()
-
+        # Child Layout #
         self.rightMainLayout.addLayout(self.rightTopLayout)
         self.rightMainLayout.addLayout(self.rightBottomLayout)
-        self.mainLayout.addLayout(self.leftLayout)
-        self.mainLayout.addLayout(self.rightMainLayout)
+        self.mainLayout.addLayout(self.leftLayout, 40)
+        self.mainLayout.addLayout(self.rightMainLayout, 60)
+        # Adding widgets #
+        self.rightTopLayout.addWidget(self.employeeList)
+        self.rightBottomLayout.addWidget(self.btnNew)
+        self.rightBottomLayout.addWidget(self.btnUpdate)
+        self.rightBottomLayout.addWidget(self.btnDelete)
 
+        # Setting layout #
         self.setLayout(self.mainLayout)
 
 
